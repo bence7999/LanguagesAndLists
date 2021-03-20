d3095352812784ddc4fca6db4d1a88ff526f8846
Tihanyi Benedek <tihanyibenedek@gmail.com>
2021-03-20 10:48:43 +0100

    Language change

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
   <div id="app">
+    <button v-for="entry in languages" :key="entry.title" @click="changeLocale(entry.language)">
+        <flag :iso="entry.flag" v-bind:squared=false /> {{entry.title}}
+    </button>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
+    <div>
+  </div>
   </div>
 </template>
 
@@ -29,4 +34,30 @@
 #nav a.router-link-exact-active {
   color: #42b983;
 }
+button {
+    padding: 15px;
+    border: 1px solid green;
+    font-size: 18px;
+    margin: 15px;
+}
 </style>
+
+<script>
+import i18n from '@/plugins/i18n';
+
+export default {
+  data() {
+    return {
+      languages: [
+        { flag: 'us', language: 'en', title: 'English' },
+        { flag: 'es', language: 'es', title: 'Español' },
+      ],
+    };
+  },
+  methods: {
+    changeLocale(locale) {
+      i18n.locale = locale;
+    },
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue';
+import i18n from '@/plugins/i18n';
+import FlagIcon from 'vue-flag-icon';
 import App from './App.vue';
 import router from './router';
 
+Vue.use(FlagIcon);
 Vue.config.productionTip = false;
 
 new Vue({
+  i18n,
   router,
   render: (h) => h(App),
 }).$mount('#app');
